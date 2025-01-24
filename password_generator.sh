e68bd248afa11e77d4dfec4d8eb158489e419aa7
Joy Kimaiyo <joy.kimaiyo18@gmail.com>
2025-01-24 10:46:41 +0300

    this is the script

--- password_generator.sh
+++ password_generator.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-check_file() {
-    if [ -e /home/joy/Documents/password_generator.sh ]; then
-        echo "Yes, it exists"
-    else
-        echo "No, it does not"
-    fi
-}
+echo "This will help you generate passwords"
+read -p "Enter the length of your passwords: " pass_length
+read -p "Enter the number of passwords to generate: " num_passwords
 
-check_file
-
+for i in $(seq 1 "$num_passwords"); do
+  openssl rand -base64 48 | cut -c1-"$pass_length" >> passwords.txt
+done
 
 
